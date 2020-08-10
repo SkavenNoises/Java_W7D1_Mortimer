6ddc7bcd56489d0ad4fbcdda91360ea1968767d3
aki <kieran.mortimer@gmail.com>
2020-08-10 10:27:03 +0200

    student table display

--- src/Main.java
+++ src/Main.java
@@ -20,26 +20,32 @@ public class Main {
 
             // Checking to see if the database has been connected to
             if (!connection.isClosed()) {
-                System.out.println("Connection Established...");
+                System.out.println("Connection Established...\n");
             }
 
             // Creating the QUERY statement object instance
             Statement statement = connection.createStatement();
 
             // Storing the SQL statement you wish to pass
-            String sqlStatement = "SELECT * FROM students";
+            String sqlStatement = "SELECT * FROM students ORDER BY students.name ASC";
 
             // Executing the statement and storing the return in an ResultSet
             ResultSet resultSet = statement.executeQuery(sqlStatement);
 
+            // Creating the output list header
+            String studentListHeader = String.format("%3s %20s", "ID", "Name");
+            System.out.println("-".repeat(studentListHeader.length()));
+            System.out.println(studentListHeader);
+            System.out.println("-".repeat(studentListHeader.length()));
+
+
             // Iterating through the result set that has been returned
             while (resultSet.next()) {
+                // Passing the info into variables
                 String studentName = resultSet.getString("name");
+                int studentID = resultSet.getInt("id");
 
-                System.out.println(studentName);
-
-
-
+                System.out.println(String.format("%3s %20s", studentID, studentName));
             }
 
             connection.close();
